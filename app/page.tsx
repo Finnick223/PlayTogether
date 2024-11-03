b2ada0fea9993e3c7c7aa3d33df8b90f560a67db
Finnick223 <filipf192@gmail.com>
2024-11-03 15:55:08 +0100

    Add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
@@ -15,13 +17,14 @@ export default function Home() {
 
         <div className='flex flex-col items-center gap-4 sm:flex-row'>
           <a
-            className='flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]'
+            className='flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base'
             href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
             target='_blank'
             rel='noopener noreferrer'
           >
             Read our docs
           </a>
+          <Button variant={'outline'}>SHADCN BUTTON</Button>
         </div>
       </main>
     </div>
